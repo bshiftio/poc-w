8104327c98b1bea4deff798b11d529f500963347
bucurdavid <dbucur22@gmail.com>
2024-11-22 23:07:02 +0200

    fix: remove tg ui

--- src/pages/LaunchParamsPage.tsx
+++ src/pages/LaunchParamsPage.tsx
@@ -1,5 +1,5 @@
 import { useLaunchParams } from "@telegram-apps/sdk-react";
-import { List } from "@telegram-apps/telegram-ui";
+
 import type { FC } from "react";
 
 import { Page } from "@/components/Page.tsx";
@@ -7,9 +7,5 @@ import { Page } from "@/components/Page.tsx";
 export const LaunchParamsPage: FC = () => {
   const lp = useLaunchParams();
 
-  return (
-    <Page>
-      <List></List>
-    </Page>
-  );
+  return <Page></Page>;
 };
--- src/pages/ThemeParamsPage.tsx
+++ src/pages/ThemeParamsPage.tsx
@@ -1,14 +1,9 @@
 import { themeParams, useSignal } from "@telegram-apps/sdk-react";
 import type { FC } from "react";
-import { List } from "@telegram-apps/telegram-ui";
 import { Page } from "@/components/Page.tsx";
 
 export const ThemeParamsPage: FC = () => {
   const tp = useSignal(themeParams.state);
 
-  return (
-    <Page>
-      <List></List>
-    </Page>
-  );
+  return <Page></Page>;
 };
