443813258ec7c9747dd2f02cc4e2d4c18c199d17
bucurdavid <dbucur22@gmail.com>
2024-11-24 22:20:53 +0200

    fix: remove header

--- src/telegram/init.ts
+++ src/telegram/init.ts
@@ -41,9 +41,4 @@ export function init(debug: boolean): void {
   // Define components-related CSS variables.
   miniApp.bindCssVars();
   themeParams.bindCssVars();
-
-  if (miniApp.setHeaderColor.isAvailable()) {
-    miniApp.setHeaderColor("#09090B");
-    miniApp.headerColor(); // 'bg_color'
-  }
 }
